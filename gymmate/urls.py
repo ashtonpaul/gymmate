cddd1b8ad59cf404a8f7edb06d5de218b21b2cea
Ashton Paul <ashton@ashtonpaul.com>
2016-01-04 22:50:11 +0000

    add admin url

--- gymmate/urls.py
+++ gymmate/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
+from django.contrib import admin
+
 from rest_framework import routers
 from accounts.views import UserViewSet
 from metrics.views import MetricViewSet, MetricTypeViewSet, MetricTypeGroupViewSet
+
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -14,4 +17,9 @@ router.register(r'metrics-type-group', MetricTypeGroupViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
+
+# Login and logout views for the browsable API
+urlpatterns += [
+    url(r'^admin/', include(admin.site.urls)),
 ]
