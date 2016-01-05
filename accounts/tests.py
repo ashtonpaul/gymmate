ccd1a29c8d0189801b3da6b0553f574957cb8512
Ashton Paul <ashton@ashtonpaul.com>
2016-01-05 17:23:45 +0000

    first user test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,15 @@
-# from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from .models import AccountUser
 
-# Create your tests here.
+
+class AccountTests(APITestCase):
+    def setUp(self):
+        self.test_user = AccountUser.objects.create_user(username='test', password='test', is_active=True)
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.test_user)
+
+    def test_create_user(self):
+        response = self.client.post('/users/', {'username': 'temp'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
