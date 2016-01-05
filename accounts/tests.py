4b4b8bcb08a96a0e736cfbb3b776b110c0b42cfd
Ashton Paul <ashton@ashtonpaul.com>
2016-01-05 22:07:02 +0000

    add tests for user profile

--- accounts/tests.py
+++ accounts/tests.py
@@ -5,10 +5,35 @@ from .models import AccountUser
 
 class AccountTests(APITestCase):
     def setUp(self):
+        """
+        Setup the test with a default user for testing use
+        """
         self.test_user = AccountUser.objects.create_user(username='test', password='test', is_active=True)
         self.client = APIClient()
         self.client.force_authenticate(user=self.test_user)
 
     def test_create_user(self):
-        response = self.client.post('/users/', {'username': 'temp'}, format='json')
+        """
+        Create a profile without authentication
+        """
+        client = APIClient()
+        response = client.post('/users/', {'username': 'temp'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_delete_other_user(self):
+        """
+        A user should not be able to delete another user's profile
+        """
+        user_one = AccountUser.objects.create_user(username='user_one', is_active=True)
+        endpoint = '/users/%d/' % user_one.id
+        response = self.client.delete(endpoint, format='json')
+        self.assertNotEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_update_another_user(self):
+        """
+        A user should not be able to update another user's profile
+        """
+        user_one = AccountUser.objects.create_user(username='user_one', is_active=True)
+        endpoint = '/users/%d/' % user_one.id
+        response = self.client.put(endpoint, {'username': 'user_one', 'email': 'test@test.com'}, format='json')
+        self.assertNotEqual(response.status_code, status.HTTP_200_OK)
