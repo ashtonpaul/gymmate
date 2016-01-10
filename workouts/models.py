e7d863a9bd148ecc90472e4ec1c808a920b8208a
Ashton Paul <ashton.paul@gmail.com>
2016-01-10 21:35:03 +0000

    update models

--- workouts/models.py
+++ workouts/models.py
@@ -3,11 +3,20 @@ from exercises.models import Exercise
 from accounts.models import AccountUser
 
 
+class DaysOfWeek(models.Model):
+    day = models.CharField(max_length=9, blank=False, null=False)
+
+    def __str__(self):
+        return self.day
+
+
 class Routine(models.Model):
     user = models.ForeignKey(AccountUser)
     name = models.CharField(max_length=100, blank=False)
     date_created = models.DateTimeField(auto_now_add=True)
     exercises = models.ManyToManyField(Exercise)
+    days = models.ManyToManyField(DaysOfWeek)
+    is_public = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['name', ]
