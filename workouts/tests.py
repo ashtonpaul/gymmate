6d86ed1c8e567295ebf2b0f9c4ce3eb76d70a395
Ashton Paul <ashton@ashtonpaul.com>
2016-01-11 22:55:22 +0000

    progress tests

--- workouts/tests.py
+++ workouts/tests.py
@@ -162,3 +162,74 @@ class ProgressTest(BaseTestCase):
         exercise = Exercise.objects.create(name='squats', description='squat', )
         progress = Progress.objects.create(user=self.test_user, exercise=exercise, date=datetime.date.today())
         self.assertEqual(str(progress), 'squats - %s' % (progress.date.strftime('%m/%d/%Y')))
+
+    def test_add_progress_non_admin(self):
+        exercise = Exercise.objects.create(name='squats', description='squat', )
+        self.create_non_admin_user()
+        response = self.client.post(
+            reverse('progress-list'),
+            {'user': self.non_admin_user.id, 'exercise': exercise.id, 'duration': '767', 'date': datetime.date.today()}
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Progress.objects.count(), 1)
+        self.assertEqual(Progress.objects.get().duration, 767)
+
+    def test_delete_progress_non_admin(self):
+        exercise = Exercise.objects.create(name='squats', description='squat', )
+
+        self.create_non_admin_user()
+        progress = Progress.objects.create(user=self.non_admin_user, exercise=exercise, date=datetime.date.today())
+        response = self.client.delete(reverse('progress-detail', args=(progress.id,)))
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Progress.objects.count(), 0)
+        self.assertRaises(Progress.DoesNotExist, lambda: Progress.objects.get(id=progress.id))
+
+    def test_delete_by_other_user(self):
+        exercise = Exercise.objects.create(name='squats', description='squat', )
+        progress = Progress.objects.create(user=self.test_user, exercise=exercise, date=datetime.date.today())
+
+        self.create_non_admin_user()
+        response = self.client.delete(reverse('progress-detail', args=(progress.id,)))
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(Progress.objects.count(), 1)
+        self.assertTrue(Progress.objects.get(id=progress.id))
+
+    def test_update_progress_non_admin(self):
+        exercise = Exercise.objects.create(name='squats', description='squat', )
+
+        self.create_non_admin_user()
+        progress = Progress.objects.create(user=self.non_admin_user, exercise=exercise,
+                                           date=datetime.date.today(), duration=767)
+        put = self.client.put(
+            reverse('progress-detail', args=(progress.id,)),
+            {'duration': '940', 'date': datetime.date.today(), 'exercise': exercise.id}
+        )
+        put_duration = Progress.objects.get().duration
+
+        patch = self.client.patch(reverse('progress-detail', args=(progress.id,)), {'duration': '214'})
+
+        self.assertEqual(put.status_code, status.HTTP_200_OK)
+        self.assertEqual(patch.status_code, status.HTTP_200_OK)
+        self.assertEqual(put_duration, 940)
+        self.assertEqual(Progress.objects.get().duration, 214)
+        self.assertEqual(Progress.objects.count(), 1)
+
+    def test_permissions(self):
+        exercise = Exercise.objects.create(name='squats', description='squat', )
+        progress = Progress.objects.create(user=self.test_user, exercise=exercise,
+                                           date=datetime.date.today(), duration=767)
+
+        self.create_non_admin_user()
+
+        delete = self.client.delete(reverse('progress-detail', args=(progress.id,)))
+        put = self.client.put(
+            reverse('progress-detail', args=(progress.id,)),
+            {'duration': '940', 'date': datetime.date.today(), 'exercise': exercise.id}
+        )
+        patch = self.client.patch(reverse('progress-detail', args=(progress.id,)), {'duration': '214'})
+
+        print delete
+        print put
+        print patch
