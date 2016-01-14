239def0ed9fb8b420ed60d05a668194239e15725
Ashton Paul <ashton@ashtonpaul.com>
2016-01-14 21:28:05 +0000

    add more test for accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,9 +1,13 @@
+import datetime
+
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
 from gymmate.tests import BaseTestCase
 from metrics.models import Metric, MetricType, MetricTypeGroup
+from exercises.models import Exercise
+from workouts.models import Routine, Progress
 
 from .models import AccountUser
 
@@ -52,6 +56,19 @@ class AccountTests(BaseTestCase):
         self.assertEqual(put.status_code, status.HTTP_200_OK)
         self.assertEqual(updated_user.username, 'updated')
 
+    def test_delete_account(self):
+        """
+        A user should be able to delete their account
+        """
+        user = self.authenticate(self.user_basic)
+        count_before_delete = AccountUser.objects.count()
+        response = self.client.delete(reverse('user-detail', args=(self.user.id,)))
+        
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertRaises(AccountUser.DoesNotExist, lambda: AccountUser.objects.get(id=user.id))
+        self.assertEqual(count_before_delete, 2)
+        self.assertEqual(AccountUser.objects.count(), 1)
+
     def test_delete_other_user(self):
         """
         A user should not be able to delete another user's profile
@@ -88,5 +105,34 @@ class AccountTests(BaseTestCase):
         metric = Metric.objects.create(user=self.user, value='75', metric_type=type)
 
         self.client.delete(reverse('user-detail', args=(self.user.id,)))
+        
         self.assertEqual(Metric.objects.count(), 0)
         self.assertRaises(Metric.DoesNotExist, lambda: Metric.objects.get(id=metric.id))
+
+    def test_routine_cascade(self):
+        """
+        Routines should be deleted when a user account is deleted
+        """
+        self.authenticate(self.user_basic)
+        routine = Routine.objects.create(user=self.user, name='mondays', )
+        count_before_delete = Routine.objects.count()
+
+        self.client.delete(reverse('routine-detail', args=(routine.id,)))
+        self.assertEqual(count_before_delete, 1)
+        self.assertRaises(Routine.DoesNotExist, lambda: Routine.objects.get(id=routine.id))
+        self.assertEqual(Routine.objects.count(), 0)
+
+    def test_progress_cascade(self):
+        """
+        User progress entries should be deleted when a user account is deleted
+        """
+        self.authenticate(self.user_basic)
+        exercise = Exercise.objects.create(name='squats', description='squat', )
+        progress = Progress.objects.create(user=self.user, exercise=exercise, date=datetime.date.today())
+        count_before_delete = Progress.objects.count()
+
+        self.client.delete(reverse('progress-detail', args=(progress.id,)))
+        self.assertEqual(count_before_delete, 1)
+        self.assertRaises(Progress.DoesNotExist, lambda: Progress.objects.get(id=progress.id))
+        self.assertEqual(Progress.objects.count(), 0)
+
