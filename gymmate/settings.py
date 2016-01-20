843b2bd19c80b5f338a6972613224d065ac213bc
Ashton Paul <ashton@ashtonpaul.com>
2016-01-20 22:45:25 +0000

    pep8

--- gymmate/settings.py
+++ gymmate/settings.py
@@ -154,10 +154,13 @@ SWAGGER_SETTINGS = {
     },
 }
 
+
 # Set callable toolbar callback to shor Django debug toolbar
 # https://stackoverflow.com/questions/10517765/django-debug-toolbar-not-showing-up/10518040#10518040
+
 def show_toolbar(request):
     return True
+
 DEBUG_TOOLBAR_CONFIG = {
-    "SHOW_TOOLBAR_CALLBACK" : show_toolbar,
+    "SHOW_TOOLBAR_CALLBACK": show_toolbar,
 }
