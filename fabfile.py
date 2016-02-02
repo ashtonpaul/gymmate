c0bb470138802efd3899c1d4549f6abbcd3b7eb0
Ashton Paul <ashton@ashtonpaul.com>
2016-02-02 22:51:33 +0000

    add fabfile for deploy

---
+++ fabfile.py
@@ -0,0 +1,27 @@
+from __future__ import with_statement
+
+import logging
+
+from fabric.api import run, sudo
+from fabric.api import env
+from fabric.network import disconnect_all
+from fabric.context_managers import cd
+
+logging.getLogger('paramiko.transport').addHandler(logging.StreamHandler())
+
+code_path = "~/www/html"
+
+env.hosts = ["staging.dnsdynamic.com"]
+env.user = "pi"
+env.password = "fuckdying1"
+
+def deploy(project_name=None):
+    sudo("apt-get install -y libmysqlclient-dev")
+    with cd(code_path):
+        run("rm -rf {0}_env".format(project_name))
+        run("rm -rf {0}".format(project_name))
+        run("virtualenv {0}_env".format(project_name))
+        run("source {0}_env/bin/activate".format(project_name))
+        run("git clone git@bitbucket.org:ashtonpaul/{0}.git".format(project_name))
+        run("{0}_env/bin/pip install -r {0}/requirements.txt".format(project_name))
+        disconnect_all()
