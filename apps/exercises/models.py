d0a32b538d16c9916e148f8aab75a67a8a6f97ac
Ashton Paul <ashton@ashtonpaul.com>
2016-02-24 22:31:08 +0000

    image delete, is_main checks

--- apps/exercises/models.py
+++ apps/exercises/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 
 class Muscle(models.Model):
@@ -70,7 +72,7 @@ def upload_to(instance, filename):
     '''
     Returns the upload target for exercise images
     '''
-    return "exercise-images/{0}/{1}".format(instance.exercise.id, filename)
+    return "exercises/{0}/{1}".format(instance.exercise.id, filename)
 
 
 class ExerciseImage(models.Model):
@@ -83,7 +85,41 @@ class ExerciseImage(models.Model):
 
     class Meta:
         ordering = ['-is_main', 'id']
+        verbose_name_plural = 'Exercise Images'
 
     def __str__(self):
         exercise = Exercise.objects.get(id=self.exercise.id)
         return '{0} - {1}'.format(exercise, self.image)
+
+    def save(self, *args, **kwargs):
+        """
+        Only one is_main image can exist per exercise
+        """
+        if self.is_main and ExerciseImage.objects.filter(exercise=self.exercise, is_main=True).count():
+            self.is_main = False
+
+        super(ExerciseImage, self).save(*args, **kwargs)
+        check_is_main(self.exercise.id)
+
+
+def check_is_main(exercise):
+    """
+    Check if an exercise has a main image, if not then default the first image as main
+    """
+    if (not ExerciseImage.objects.filter(exercise=exercise, is_main=True).count() and
+       ExerciseImage.objects.filter(exercise=exercise, is_main=False).count() != 0):
+        image = ExerciseImage.objects.filter(exercise=exercise, is_main=False)[0]
+        image.is_main = True
+        image.save()
+
+
+@receiver(post_delete, sender=ExerciseImage)
+def exercise_image_post_delete(sender, **kwargs):
+    """
+    Post delete hook to delete the physical file
+    """
+    file = kwargs['instance']
+    storage = file.image.storage
+    path = file.image.path
+    storage.delete(path)
+    check_is_main(file.exercise.id)
