7efbf9576a71783db494bee70bc77414900d3ce7
Ashton Paul <ashton.paul@gmail.com>
2016-03-15 22:56:16 +0000

    add celery accept content settings

--- gymmate/settings/production.py
+++ gymmate/settings/production.py
@@ -10,9 +10,12 @@ ALLOWED_HOSTS = [".herokuapp.com", ".jusdev.com"]
 # RabbitMQ Broker settings
 # http://www.marinamele.com/2014/02/how-to-install-celery-on-django-and.html
 # http://www.marinamele.com/2014/03/install-celery-with-django-on-heroku.html
+
 BROKER_URL =  get_secret("CLOUDAMQP_URL")
 BROKER_POOL_LIMIT = 20
+
 
 CELERY_RESULT_BACKEND = "amqp://"
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
+CELERY_ACCEPT_CONTENT = ['json', 'msgpack', 'yaml']
