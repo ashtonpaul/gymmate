fc83548040db00d7d5ef087662540e9ccdaf7732
Ashton Paul <ashton.paul@gmail.com>
2016-03-15 19:46:06 +0000

    remove deprecated patterns

--- gymmate/urls.py
+++ gymmate/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include, patterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.conf import settings
 
@@ -52,6 +53,5 @@ urlpatterns += [
 ]
 
 # status files
-urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+urlpatterns += staticfiles_urlpatterns()
+
