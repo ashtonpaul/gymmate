dc5b279f44e64ff045658aaff2c4cf15d373486b
Ashton Paul <ashton.paul@gmail.com>
2016-03-15 23:25:27 +0000

    pep8

--- gymmate/urls.py
+++ gymmate/urls.py
@@ -52,6 +52,7 @@ urlpatterns += [
 ]
 
 # status files
-urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+urlpatterns += patterns(
+    '',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
