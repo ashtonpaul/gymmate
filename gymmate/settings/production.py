bb3044fc6195c1d2c2d7b8c9550c914d348bd9fd
Ashton Paul <ashton.paul@gmail.com>
2016-03-15 04:35:50 +0000

    production strings and database url

--- gymmate/settings/production.py
+++ gymmate/settings/production.py
@@ -1,13 +1,17 @@
 # Settings for production environment
+import dj_database_url
+
 from .base import *
+
+# production database url
+# https://github.com/kennethreitz/dj-database-url#usage
+
+DATABASES['default'] = dj_database_url.config(default=get_secret("JAWSDB_URL"))
+
 
 # RabbitMQ Broker settings
 # http://www.marinamele.com/2014/02/how-to-install-celery-on-django-and.html
-BROKER_HOST = "localhost"
-BROKER_PORT = 5672
-BROKER_USER = get_secret("RABBITMQ_USER")
-BROKER_PASSWORD = get_secret("RABBITMQ_PASSWORD")
-BROKER_VHOST = "/"
+BROKER_URL =  get_secret("CLOUDAMQP_URL")
 
 CELERY_BACKEND = "amqp" 
 CELERY_RESULT_DBURI = ""
