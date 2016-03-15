de94c5f653e9f7e492d423656a2bf5bc4b8d5d52
Ashton Paul <ashton.paul@gmail.com>
2016-03-15 04:51:32 +0000

    remove dj-db-url

--- gymmate/settings/production.py
+++ gymmate/settings/production.py
@@ -1,13 +1,5 @@
 # Settings for production environment
-import dj_database_url
-
 from .base import *
-
-# production database url
-# https://github.com/kennethreitz/dj-database-url#usage
-
-DATABASES['default'] = dj_database_url.config(default=get_secret("JAWSDB_URL"))
-
 
 # RabbitMQ Broker settings
 # http://www.marinamele.com/2014/02/how-to-install-celery-on-django-and.html
